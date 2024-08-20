e94a6f2ae85d4280475a890e14783163bc37a199
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-20 15:26:25 +0000

    type annotated the function to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    3-to_str.py: Basic annotations - to string
+
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        Return: the string repr of n
+
+    """
+
+    return str(n)

ccd42162c58deed4f55f4ed3b782a511aad3b1cf
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-20 15:11:00 +0000

    type-annotated the function add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    0-add.py: basic annotations
+
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        add float
+
+        Return: float
+    """
+
+    return a + b

d71c9141040f455a1d37e006ed43908531c06598
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-20 13:20:58 +0000

    concurrent coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+"""
+    1-concurrent_coroutines.py:
+    execute multiple coroutines at the same time with async
+
+"""
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """
+        async routine that return the list of all the delays by wait_random
+    """
+    delays = []
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+
+    return delays

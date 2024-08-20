2763a2845002d61ad8b948db26335dbc381265b9
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-20 15:20:43 +0000

    type annotated the function floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    2-floor.py: Basic annotations - floor
+
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+        Return: floor of n
+
+    """
+
+    return math.floor(n)

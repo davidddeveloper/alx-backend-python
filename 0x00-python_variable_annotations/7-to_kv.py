aebaa9eecc064786d9d465f028166080fca22130
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-20 16:09:12 +0000

    type annotated to_kv

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,7 +7,7 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple([str, float]):
     """
         - Args:
             - k: the string

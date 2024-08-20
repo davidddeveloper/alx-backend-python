2078b9efb6704158b7308763600e86dec3861908
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-20 16:24:33 +0000

    type annotated make_multiplier that ruturns a function

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+    8-make_multiplier.py: Complex types - functions
+
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable:
+    """
+        - Args:
+            - multiplier: float
+
+        - Return: a function that multiplies a float by multiplier
+
+    """
+
+    def mp(mtip):
+        return mtip * multiplier
+
+    return mp

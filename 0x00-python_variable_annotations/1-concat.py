a8026e414f08e2f0002d0aba8333a584dc97b816
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-20 15:14:16 +0000

    type-annotated the function concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    1-concat.py: Basic annotations - concat
+
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Return: concated string of str1 and str2
+
+    """
+
+    return str1 + str2
